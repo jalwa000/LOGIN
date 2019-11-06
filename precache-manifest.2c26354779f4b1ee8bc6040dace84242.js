ae0668da1d5433043ff562a58c8eb97499f0bc94
khattak <jalwaus2000@googlemail.com>
2019-11-06 11:02:50 +0100

    Updates

---
+++ precache-manifest.2c26354779f4b1ee8bc6040dace84242.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1bf40e95a18b41ff2d3d25960878499",
+    "url": "/login/index.html"
+  },
+  {
+    "revision": "20b121812aba50d78809",
+    "url": "/login/static/js/2.bf8de51e.chunk.js"
+  },
+  {
+    "revision": "2566992d8046c0186fe0",
+    "url": "/login/static/js/main.704f5807.chunk.js"
+  },
+  {
+    "revision": "6a6b784d83f30b767e65",
+    "url": "/login/static/js/runtime-main.256cf3b3.js"
+  }
+]);
